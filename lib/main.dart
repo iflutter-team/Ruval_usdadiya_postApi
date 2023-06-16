188ccb289231fb7e86b648d0ee08969af16a32cf
Ruvalusadadiya <124563709+Ruvalusadadiya@users.noreply.github.com>
2023-06-16 11:34:46 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: LoginScreen(),
     );
   }
